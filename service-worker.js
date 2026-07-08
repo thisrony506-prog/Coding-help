8518646f25e2a985f4230656f3030db383847f5a
thisrony506-prog <thisrony506@gmail.com>
2026-07-09 01:25:23 +0600

    Change cache version and simplify service worker
    Updated cache version and removed unnecessary activation event.

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,8 @@
-const CACHE = 'devstudio-v1.1';
+const CACHE = 'devstudio-mobile-v1';
 const ASSETS = ['./', './index.html', './style.css', './script.js', './manifest.json'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', e => {
